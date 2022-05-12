c106c7fada727b3af228e069d7a597b751525b7c
KelvinP25 <kelvincastillo9911@gmail.com>
2022-05-12 00:41:45 -0400

    Creando el Fragment de Registro de personas

---
+++ app/src/main/java/com/example/registropersonas/Views/RegistroPersonas/RegistroPersonasViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.registropersonas.Views.RegistroPersonas
+
+import androidx.lifecycle.ViewModel
+
+class RegistroPersonasViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
