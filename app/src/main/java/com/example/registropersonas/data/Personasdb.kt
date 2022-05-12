38865ddf2285081956e587449d894c165c04a063
KelvinP25 <kelvincastillo9911@gmail.com>
2022-05-12 10:22:00 -0400

    Creando la clase abtracta de Personasdb

---
+++ app/src/main/java/com/example/registropersonas/data/Personasdb.kt
@@ -0,0 +1,13 @@
+package com.example.registropersonas.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.registropersonas.model.Persona
+
+@Database(
+    entities = [Persona::class],
+    version = 1
+)
+abstract class Personasdb: RoomDatabase() {
+    abstract val personaDao: Persona
+}
