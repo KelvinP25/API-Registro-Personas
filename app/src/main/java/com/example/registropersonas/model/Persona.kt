babe8980f127b3587e4bdb3296590ef42e7aeaec
KelvinP25 <kelvincastillo9911@gmail.com>
2022-05-12 09:38:22 -0400

    Creando la data clas de personas

---
+++ app/src/main/java/com/example/registropersonas/model/Persona.kt
@@ -0,0 +1,13 @@
+package com.example.registropersonas.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName ="Personas")
+data class Persona(
+    @PrimaryKey(autoGenerate = true)
+    val personaId: Int,
+    val email: String,
+    val ocupacioId: Int,
+    val salario: Float
+)
